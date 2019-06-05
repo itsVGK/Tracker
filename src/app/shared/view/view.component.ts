015fab1a056238210cc88d118027a5ac01d5c18c
Gopalakrishnan <gvenkidu@its.jnj.com>
2019-06-05 20:11:41 +0530

    5/6

--- src/app/shared/view/view.component.ts
+++ src/app/shared/view/view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 
 import { AppService } from './../../app.service';
 import { Router, ActivatedRoute } from '@angular/router';
